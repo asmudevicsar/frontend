c4a17feaf4f9129686a654f23ecc83d03ee89548
asmudevicsar <106889794+asmudevicsar@users.noreply.github.com>
2022-07-01 10:44:15 -0500

    Update index.js

--- src/components/quienesSomos/MisionVision/index.js
+++ src/components/quienesSomos/MisionVision/index.js
@@ -13,14 +13,14 @@ export default function MisionVision({ title, description, objetivos = '', visio
           {imageMision && <img alt="image" className="w-32 h-28 m-auto" src={imageMision} />}
           <div className="pt-7 px-2">
             <h3 className="text-center font-bold text-purpledark text-2xl mb-2">Misión</h3>
-            <p className="text-base text-[#606060] m-auto text-center leading-5 px-4  break-words">{mision}</p>
+            <p className="text-base text-[#606060] m-auto text-center leading-6 px-4  break-words">{mision}</p>
           </div>
         </div>
         <div className="bg-white rounded-3xl border-2 border-[#9D9A9B] cursor-pointer py-10">
           {imageVision && <img alt="image" className="w-32 h-28 m-auto" src={imageVision} />}
           <div className="pt-7 px-2">
             <h3 className="text-center font-bold text-purpledark text-2xl mb-2">Visión</h3>
-            <p className="text-base text-[#606060] m-auto text-center leading-5 px-4  break-words">{vision}</p>
+            <p className="text-base text-[#606060] m-auto text-center leading-6 px-4  break-words">{vision}</p>
           </div>
         </div>
       </div>
