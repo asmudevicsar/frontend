1342b104f5ba61c950121b6d0b683c0d8a0d8c91
asmudevicsar <106889794+asmudevicsar@users.noreply.github.com>
2023-02-06 08:51:07 -0500

    Update index.js

--- src/components/generalComponents/BannerInterno/index.js
+++ src/components/generalComponents/BannerInterno/index.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { getBannersInternos, getBannersInternosCategory } from 'api/bannerInternosAPI';
-import { useRouter } from 'next/router';
-import { API_URL } from 'utils/constants';
-import Loader from '@common/Loader';
+import React, { useState, useEffect } from "react";
+import {
+  getBannersInternos,
+  getBannersInternosCategory,
+} from "api/bannerInternosAPI";
+import { useRouter } from "next/router";
+import { API_URL } from "utils/constants";
+import Loader from "@common/Loader";
 
 export default function BannerInterno({ title, img, description }) {
   const query = useRouter();
@@ -14,22 +17,44 @@ export default function BannerInterno({ title, img, description }) {
       /* Obtener los datos de la API y luego mapearlos para obtener los datos que necesito. */
       const response = await getBannersInternos();
       response?.data?.map((item) => {
-        if (item?.attributes?.Pagina?.data?.attributes?.Nombre == 'Quienes Somos' && query.pathname.includes('/quienes-somos')) {
+        if (
+          item?.attributes?.Pagina?.data?.attributes?.Nombre ==
+            "Quienes Somos" &&
+          query.pathname.includes("/quienes-somos")
+        ) {
           setDataBanner(item?.attributes);
         }
-        if (item?.attributes?.Pagina?.data?.attributes?.Nombre == 'Proyectos' && query.pathname.includes('/proyectos')) {
+        if (
+          item?.attributes?.Pagina?.data?.attributes?.Nombre == "Proyectos" &&
+          query.pathname.includes("/proyectos")
+        ) {
           setDataBanner(item?.attributes);
         }
-        if (item.attributes?.Pagina?.data?.attributes?.Nombre == 'Galeria' && query.pathname.includes('/galeria')) {
+        if (
+          item.attributes?.Pagina?.data?.attributes?.Nombre == "Galeria" &&
+          query.pathname.includes("/galeria")
+        ) {
           setDataBanner(item?.attributes);
         }
-        if (item?.attributes?.Pagina?.data?.attributes?.Nombre == 'Eventos y Noticias' && query.pathname.includes('/centros-de-prensa/eventos-y-noticias')) {
+        if (
+          item?.attributes?.Pagina?.data?.attributes?.Nombre ==
+            "Eventos y Noticias" &&
+          query.pathname.includes("/centros-de-prensa/eventos-y-noticias")
+        ) {
           setDataBanner(item?.attributes);
         }
-        if (item?.attributes?.Pagina?.data?.attributes?.Nombre == 'Comunicados de Prensa' && query.pathname.includes('/centros-de-prensa/comunicados-de-prensa')) {
+        if (
+          item?.attributes?.Pagina?.data?.attributes?.Nombre ==
+            "Comunicados de Prensa" &&
+          query.pathname.includes("/centros-de-prensa/comunicados-de-prensa")
+        ) {
           setDataBanner(item?.attributes);
         }
-        if (item?.attributes?.Pagina?.data?.attributes?.Nombre == 'Informe Corporativo Mensual' && query.pathname.includes('/quienes-somos/informe-corporativo-mensual')) {
+        if (
+          item?.attributes?.Pagina?.data?.attributes?.Nombre ==
+            "Informe Corporativo Mensual" &&
+          query.pathname.includes("/quienes-somos/informe-corporativo-mensual")
+        ) {
           setDataBanner(item?.attributes);
         }
       });
@@ -40,11 +65,14 @@ export default function BannerInterno({ title, img, description }) {
           query.asPath.includes(
             `/proyectos/${itemProject?.attributes?.Categorias_de_proyectos?.data?.attributes?.Nombre_de_Categoria.trim()
               .toLowerCase()
-              .normalize('NFD')
-              .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi, '$1')
+              .normalize("NFD")
+              .replace(
+                /([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+                "$1"
+              )
               .normalize()
               .trim()
-              .replace(/ /g, '')}`
+              .replace(/ /g, "")}`
           )
         ) {
           setDataBanner(itemProject?.attributes);
@@ -53,16 +81,27 @@ export default function BannerInterno({ title, img, description }) {
       });
     })();
   }, [query]);
-  let backgroundImage = dataBanner.Imagen_de_fondo.data[0] ? dataBanner?.Imagen_de_fondo?.data[0]?.attributes?.url : dataBanner?.Imagen_de_fondo?.data?.attributes?.url;
   return dataBanner ? (
     <div
-      style={{ backgroundImage: `url(${API_URL}${backgroundImage})` }}
-      className={'relative mt-8 lg:mt-24 banner-interno active-anim image-banner h-52'}
+      style={{
+        backgroundImage: `url(${API_URL}${
+          dataBanner.Imagen_de_fondo.data[0]
+            ? dataBanner?.Imagen_de_fondo?.data[0]?.attributes?.url
+            : dataBanner?.Imagen_de_fondo?.data?.attributes?.url
+        })`,
+      }}
+      className={
+        "relative mt-8 lg:mt-24 banner-interno active-anim image-banner h-52"
+      }
     >
       <div className="content-image">
         <div className="!w-full center-data  md:!w-3/4 lg:!w-1/2">
-          <h3 className=" text-center font-bold text-xl sm:text-4xl text-white">{title ? title : dataBanner?.Titulo}</h3>
-          <p className="mt-3 text-center font-semibold text-sm xl:text-base text-white !leading-5">{description ? description : dataBanner?.Descripcion}</p>
+          <h3 className=" text-center font-bold text-xl sm:text-4xl text-white">
+            {title ? title : dataBanner?.Titulo}
+          </h3>
+          <p className="mt-3 text-center font-semibold text-sm xl:text-base text-white !leading-5">
+            {description ? description : dataBanner?.Descripcion}
+          </p>
         </div>
       </div>
     </div>
