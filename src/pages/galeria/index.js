ba9bd9b722b2b0e1ae418c7d8ba643d3736ddebf
asmudevicsar <106889794+asmudevicsar@users.noreply.github.com>
2023-02-06 08:24:01 -0500

    Update index.js

--- src/pages/galeria/index.js
+++ src/pages/galeria/index.js
@@ -28,8 +28,6 @@ export default function Gallery() {
       <Seo title="Galería de imágenes" description="Galería de imágenes" />
       <BannerInterno
         title="Galeria"
-        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQNiKhuv0pfwFxwLB2idvrmaubdad0Fp9KYQ&usqp=CAU"
-        description="Quisque nisl metus, placerat nec velit non, elementum ornare quam. Curabitur egestas blandit tempus. Pellentesque condimentum arcu quis consequat convallis. Cras ornare felis in diam gravida, vel auctor ante efficitur."
       />
       <div className="container py-8 sm:py-16 px-8 sm:px-0">
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-7">
