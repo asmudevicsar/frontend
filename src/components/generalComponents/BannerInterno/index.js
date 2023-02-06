ba179f41814076ba75adfaa68ea5a8593c7439e2
asmudevicsar <106889794+asmudevicsar@users.noreply.github.com>
2023-02-06 08:45:23 -0500

    Update index.js

--- src/components/generalComponents/BannerInterno/index.js
+++ src/components/generalComponents/BannerInterno/index.js
@@ -55,7 +55,7 @@ export default function BannerInterno({ title, img, description }) {
   }, [query]);
   return dataBanner ? (
     <div
-      style={{ backgroundImage: `url(${API_URL}${dataBanner?.Imagen_de_fondo?.data[0] ? dataBanner?.Imagen_de_fondo?.data[0]?.attributes?.url : dataBanner?.Imagen_de_fondo?.data?.attributes?.url})` }}
+      style={{ backgroundImage: `url(${API_URL}${dataBanner.Imagen_de_fondo.data[0] ? dataBanner?.Imagen_de_fondo?.data[0]?.attributes?.url : dataBanner?.Imagen_de_fondo?.data?.attributes?.url})` }}
       className={'relative mt-8 lg:mt-24 banner-interno active-anim image-banner h-52'}
     >
       <div className="content-image">
